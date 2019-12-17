fbdc02959ff2c19f8549de072b55b5e613621765
Connie <conniey@microsoft.com>
2019-12-16 17:00:13 -0800

    Add first data class.

---
+++ strength-tracker-common/src/main/java/org/powerlifting/common/models/Exercise.java
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+package org.powerlifting.common.models;
+
+public class Exercise {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
